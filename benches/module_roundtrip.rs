6134cce2b03ae827e7183150feef15ab823592ea
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2025-08-19 09:02:06 +0000

    add transform

--- benches/module_roundtrip.rs
+++ benches/module_roundtrip.rs
@@ -1,41 +1,70 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs;
 use std::hint::black_box;
-use wasmparser::Operator;
-use wirm::{
-    ir::{
-        function::FunctionBuilder,
-        types::{Body, Instruction},
-    },
-    DataType, Module,
-};
 
-fn module_roundtrip_benchmark(c: &mut Criterion) {
+fn roundtrip_wirm(c: &mut Criterion) {
     // Read the user.wasm file
     let wasm_bytes = fs::read("user.wasm").expect("Failed to read user.wasm");
 
-    c.bench_function("module_roundtrip", |b| {
+    c.bench_function("roundtrip_wirm", |b| {
         b.iter(|| {
+            let instant = std::time::Instant::now();
             // Parse the module
             let mut module =
-                Module::parse(black_box(&wasm_bytes), false).expect("Failed to parse module");
+                wirm::Module::parse(black_box(&wasm_bytes), false).expect("Failed to parse module");
 
-            let import_id = module
-                .imports
-                .find("ic0".to_string(), "time".to_string())
-                .unwrap();
-            let mut func = FunctionBuilder::new(&[], &[DataType::I64]);
-            let instructions = vec![Instruction::new(Operator::I64Const { value: 1 })];
-            func.body = Body {
-                locals: vec![],
-                num_locals: 0,
-                num_instructions: instructions.len(),
-                instructions,
-                name: None,
-            };
-            func.replace_import_in_module(&mut module, import_id);
+            println!("wirm parse: {:?}", instant.elapsed());
+            // let import_id = module
+            //     .imports
+            //     .find("ic0".to_string(), "time".to_string())
+            //     .unwrap();
+            // let mut func = FunctionBuilder::new(&[], &[DataType::I64]);
+            // let instructions = vec![Instruction::new(Operator::I64Const { value: 1 })];
+            // func.body = Body {
+            //     locals: vec![],
+            //     num_locals: 0,
+            //     num_instructions: instructions.len(),
+            //     instructions,
+            //     name: None,
+            // };
+            // func.replace_import_in_module(&mut module, import_id);
             // Emit the module back to bytes
             let emitted_bytes = module.encode();
+
+            // Return the emitted bytes to prevent optimization
+            black_box(emitted_bytes)
+        });
+    });
+}
+
+fn roundtrip_transform(c: &mut Criterion) {
+    // Read the user.wasm file
+    let wasm_bytes = fs::read("user.wasm").expect("Failed to read user.wasm");
+
+    c.bench_function("roundtrip_transform", |b| {
+        b.iter(|| {
+            let instant = std::time::Instant::now();
+            // Parse the module
+            let module = wirm::wasm_transform::Module::parse(black_box(&wasm_bytes), false)
+                .expect("Failed to parse module");
+            println!("transform parse: {:?}", instant.elapsed());
+
+            // let import_id = module
+            //     .imports
+            //     .find("ic0".to_string(), "time".to_string())
+            //     .unwrap();
+            // let mut func = FunctionBuilder::new(&[], &[DataType::I64]);
+            // let instructions = vec![Instruction::new(Operator::I64Const { value: 1 })];
+            // func.body = Body {
+            //     locals: vec![],
+            //     num_locals: 0,
+            //     num_instructions: instructions.len(),
+            //     instructions,
+            //     name: None,
+            // };
+            // func.replace_import_in_module(&mut module, import_id);
+            // Emit the module back to bytes
+            let emitted_bytes = module.encode().unwrap();
 
             // Return the emitted bytes to prevent optimization
             black_box(emitted_bytes)
@@ -46,6 +75,6 @@ fn module_roundtrip_benchmark(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = module_roundtrip_benchmark
+    targets = roundtrip_wirm, roundtrip_transform
 }
 criterion_main!(benches);
