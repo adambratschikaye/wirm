3ed2b67a0749f12e6374a72c468dd8c2eba3cf5f
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2025-08-25 12:21:45 +0000

    record parse and encode time in example

--- examples/wirm.rs
+++ examples/wirm.rs
@@ -2,12 +2,21 @@ use std::{fs, hint::black_box};
 
 fn main() {
     let wasm_bytes = fs::read("user.wasm").expect("Failed to read user.wasm");
+    let mut total_parse = std::time::Duration::from_secs(0);
+    let mut total_encode = std::time::Duration::from_secs(0);
 
-    let instant = std::time::Instant::now();
     for _ in 0..10 {
-        let module =
+        let instant = std::time::Instant::now();
+        let mut module =
             wirm::Module::parse(black_box(&wasm_bytes), false).expect("Failed to parse module");
-        let _ = black_box(module);
+        total_parse += instant.elapsed();
+        let instant = std::time::Instant::now();
+        let encoded = module.encode();
+        total_encode += instant.elapsed();
+        let _ = black_box(encoded);
     }
-    println!("wirm parse: {:?}", instant.elapsed());
+    println!(
+        "wirm parse time {:?}, encode time {:?}",
+        total_parse, total_encode
+    );
 }
